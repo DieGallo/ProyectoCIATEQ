b93197c30e13c9c1dc960fb7c1aa3bd4a27d387b
DieGallo <diego5julio2001@gmail.com>
2024-11-12 09:43:46 -0600

    -Se terminaron los Modelos -Se agregaron las tablas intermedias

--- ProyectoCIATEQ_api/api/models.py
+++ ProyectoCIATEQ_api/api/models.py
@@ -5,9 +5,81 @@ class Employee(models.Model):
     name = models.CharField(max_length=100)
     lastName = models.CharField(max_length=100)
     birthdate = models.DateField()
-    sex = models.BooleanField()
+    sex = models.CharField(max_length=10)
     phone = models.CharField(max_length=15)
     email = models.CharField(max_length=50)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
+    status = models.BooleanField(default=True)
+
+class Studies(models.Model):
+    description =  models.CharField(max_length=400)
+
+class Specialty(models.Model):
+    name = models.TextField(max_length=100)
+
+class Student(models.Model):
+    expedition = models.DateTimeField()
+    name = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    birthdate = models.DateField()
+    sex = models.CharField(max_length=10)
+    phone = models.CharField(max_length=15)
+    email = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    university = models.CharField(max_length=50)
+    status = models.BooleanField(default=True)
+
+class Unities(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+
+class Area(models.Model):
+    name = models.CharField(max_length=100)
+
+class Proyects(models.Model):
+    name = models.CharField(max_length=100)
+    startDate = models.DateField()
+    endDate = models.DateField()
+
+class Events(models.Model):
+    name = models.CharField(max_length=100)
+    status = models.BooleanField(default=True)
+    startDate = models.DateField()
+    endDate = models.DateField()
+
+class LineInv(models.Model):
+    name = models.CharField(max_length=100)
+
+class Articles(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+
+## Tablas Intermedias ##
+# DetProyect - Hace relación con Empleado y Proyectos
+class DetProyect(models.Model):
+    proyect = models.ForeignKey(Proyects, on_delete=models.CASCADE)
+    workers = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    students = models.ForeignKey(Student, on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+
+# DetEvent - Hace relación con Empleado y Eventos
+class DetEvent(models.Model):
+    events = models.ForeignKey(Events, on_delete=models.CASCADE)
+    workers = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+    date = models.DateField()
+
+# DetLInvestigation - Hace relación con Empleado y LInvestigación
+class DetInvestigation(models.Model):
+    workers = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    research = models.ForeignKey(LineInv, on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+
+class DetArticle(models.Model):
+    workers = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    articles = models.ForeignKey(Articles, on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
